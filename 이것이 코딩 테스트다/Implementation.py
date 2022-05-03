de2b0092d98de7de0cda9b2a5db71e53f8c33783
wndms710 <wndms710@naver.com>
2022-05-03 16:05:20 +0900

    2022-05-03 commit

--- 이것이 코딩 테스트다/Implementation.py
+++ 이것이 코딩 테스트다/Implementation.py
@@ -2,6 +2,9 @@
 
 # 예제 4-1 상하좌우
 
+from msilib import Directory
+
+
 n = int(input())
 plans = input().split()
 result = [1, 1]
@@ -92,16 +95,204 @@ print(count)
 
 # 왕실의 나이트
 
+# 문제 해설
+
+# 현재 나이트의 위치 입력받기
 coordinate = input()
-x, y = coordinate[0], int(coordinate[1])
+row = int(coordinate[1])
+collumn = int(ord(coordinate[0])) - int(ord('a')) + 1
+
+# 나이트가 이동할 수 있는 8가지 방향 정의
+steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+
+# 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
+result = 0
+for step in steps :
+    drow = row + step[0]
+    dcollumn = collumn + step[1]
+    if drow >= 1 and dcollumn >= 1 and drow <= 8 and dcollumn <= 8 :
+        result += 1
+
+print(result)
+
+
+# 게임 개발
+
+# 문제 해설
+
+n, m = map(int, input().split())
+
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+
+x, y, direction = map(int, input().split())
+d[x][y] = 1     # 현재 좌표 방문 처리
+
+# 전체 맵 정보를 입력받기
+array = []
+for i in range(m) :
+    array.append(list(map(int, input().split())))
+
+# 북, 동, 남, 서 방향 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 왼쪽으로 회전
+def turn_left() :
+    global direction
+    direction -= 1
+    if direction == -1 :
+        direction = 3
+
+# 시뮬레이션 시작
+count = 1
+turn_time = 0
+while True :
+    # 왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0 :
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+    # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else :
+        turn_time += 1
+    # 네 방향 모두 갈 수 없는 경우
+    if turn_time == 4 :
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny] == 0 :
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else :
+            break
+        turn_time = 0
+
+print(count)
+
+
+# 복습
+
+# 예제 4-1 상하좌우
+
+n = int(input())
+x, y = 1, 1
+plans = list(input().split())
+
+# 각 방향
+move_types = ['L', 'R', 'U', 'D']
+
+# 각 방향에 대한 좌표 변화
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+for plan in plans :
+    for i in range(len(move_types)) :
+       if plan == move_types[i] :
+           nx = x + dx[i]
+           ny = y + dy[i]
+    if nx < 1 or nx > n or ny < 1 or ny > n :
+        continue
+    else :
+        x = nx
+        y = ny
+
+print(x, y)
+
+
+# 예제 4-2 시각
+
+n = int(input())
 result = 0
 
-row = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-collum = [1, 2, 3, 4, 5, 6, 7, 8]
+for i in range(60):
+    for j in range(60):
+        for k in range(0, n+1) :
+            if '3' in str(i) + str(j) + str(k) :
+                result += 1
 
-# 각 행동에 따른 좌표 변화
-dx = [2, 1]
-dy = [1, 2]
+print(result)
 
-for i in range(8) :
+
+# 왕실의 나이트
+
+s = input()
+
+# 좌표로 바꾸기
+x = int(s[1])
+y = int(ord(s[0])) - int(ord('a')) + 1
+count = 0
+
+steps_type = [(-2, -1), (2, -1), (-2, 1), (2, 1), (-1, -2), (1, -2), (-1, 2), (1, 2)]
+
+for step in steps_type :
+    nx = x + step[0]
+    ny = y + step[1]
+    if nx >= 1 and nx <= 8 and ny >= 1 and ny <= 8 :
+        count += 1
     
+print(count)
+
+
+# 게임 개발
+
+n, m = map(int, input().split())
+x, y, direction = map(int, input().split())
+
+# 방문한 땅을 표시하기 위한 배열 생성
+d = [[0] * m for _ in range(n)]     # 리스트 컴프리헨션, 리스트 축약 표현
+d[x][y] = 1     # 현재 좌표 방문 처리
+
+# 맵 배열 생성
+array = []
+for i in range(n) :
+    array.append(list(map(int, input().split())))
+
+# 북, 동, 남, 서 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 왼쪽으로 회전
+def turn_left():
+    global direction    # 전역변수이기에 global 키워드 사용
+    direction -= 1
+    if direction == -1:
+        direction = 3
+
+# 시뮬레이션
+turn_time = 0
+count = 1
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    # 방문하지 않은 땅의 경우
+    if d[nx][ny] == 0 and array[nx][ny] == 0 :
+        d[nx][ny] = 1
+        x, y = nx, ny
+        turn_time = 0
+        count += 1
+    # 방문한 땅이거나 바다인 경우
+    else:
+        turn_time += 1
+    # 네 방향 모두 불가인 경우
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동
+        if array[nx][ny] == 0:
+            x, y = nx, ny
+        # 뒤가 바다인 경우
+        else:
+            break
+        turn_time = 0
+
+print(count)
