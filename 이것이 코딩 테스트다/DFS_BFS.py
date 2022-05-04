173b5306a0b99fb1f878b296e840584cee13a5c6
wndms710 <wndms710@naver.com>
2022-05-05 01:46:39 +0900

    2022-05-05 commit

---
+++ 이것이 코딩 테스트다/DFS_BFS.py
@@ -0,0 +1,108 @@
+# Chapter05. DFS/BFS
+
+# 팩토리얼 예제
+
+def factorial_func(n) :
+    if n <= 1:
+        return 1
+    return n * factorial_func(n-1)
+
+print(factorial_func(5))
+
+
+# DFS 예제
+
+# DFS 메소드 정의
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end = ' ')
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+dfs(graph, 1, visited)
+
+
+# BFS 예제
+
+from collections import deque
+import imp
+
+# BFS 정의
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end = ' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+bfs(graph, 1, visited)
+
+
+# 실전문제
+
+# 3 음료수 얼려 먹기
+
+from collections import deque
+
+n, m = map(int, input().split())
+count = 0
+
+def bfs(graph, start1, start2, visited):
+    global count
+    queue = deque([start1, start2])
+    visited[start1][start2] = True
+    v = queue.popleft()
+    while queue:
+        for i in visited:
+            for j in visited[i]:
+                if not visited[i][j]:
+                    count += 1
+                    visited[i][j] = True
+                    queue.append(i, j)
+    
+
+graph = 
