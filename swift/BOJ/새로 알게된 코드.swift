a840290073ac357d441bdef89b88e87ca98bda5b
Jueun Cho <92866097+wndms710@users.noreply.github.com>
2022-08-23 20:52:42 +0900

    Update 새로 알게된 코드.swift

--- swift/BOJ/새로 알게된 코드.swift
+++ swift/BOJ/새로 알게된 코드.swift
@@ -9,3 +9,11 @@ Array(repeating: [Int](), count: 4)
 print(message, terminator: " ")
 array.removeLast()
 array.forEach { print($0, terminator: " ") }
+
+let array = (1...p).map { _ in self }
+array.reduce(1, * )
+// reduce(1, *) : (((((1*array[0])*array[1])*array[2])*array[3])*array[4]) …
+// reduce(0, +) : (((((0+array[0])+array[1])+array[2])+array[3])+array[4]) …
+// 재귀적으로 호출
+
+let once = seen.filter { $0.value == 1}
