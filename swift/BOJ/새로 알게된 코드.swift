27c141a233ce069170f28ad7133432da53415e91
Jueun Cho <92866097+wndms710@users.noreply.github.com>
2022-08-23 18:17:49 +0900

    Update and rename 새로 알게된 코드 to 새로 알게된 코드.swift

--- swift/BOJ/새로 알게된 코드.swift
+++ swift/BOJ/새로 알게된 코드.swift
@@ -1,10 +1,11 @@
-###2022-08-23
-'''swift
+//MARK: - 2022-08-23
+
 readLine()
 split(separator: " ")
 map { Int($0)! }
+// readLine()!.split(separator: " ").map { Int($0)! }
+
 Array(repeating: [Int](), count: 4)
 print(message, terminator: " ")
 array.removeLast()
 array.forEach { print($0, terminator: " ") }
-'''
